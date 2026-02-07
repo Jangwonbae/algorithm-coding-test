d259e5b1790d28e91ae800565a953a7984b73691
Jangwonbae <84276491+Jangwonbae@users.noreply.github.com>
2026-02-07 15:58:29 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 62.9 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/12985. 예상 대진표/예상 대진표.kt
+++ Kotlin/프로그래머스/2/12985. 예상 대진표/예상 대진표.kt
@@ -3,21 +3,12 @@ class Solution {
         var answer = 1
         var x = a
         var y = b
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        println("Hello Kotlin")
 
         while((x+1)/2 != (y+1)/2) {
             x = (x+1)/2
             y = (y+1)/2
             answer++
         }
-        
-        // 1 2 3 4 5 6 7 8 //1
-        // 1 1 2 2 3 3 4 4
-        // 1 1 1 1 2 2 2 2
-        // 1 1 1 1 1 1 1 1
-        // 1 2 3 4 //2
-        // 1 2 //3
         
         return answer
     }
