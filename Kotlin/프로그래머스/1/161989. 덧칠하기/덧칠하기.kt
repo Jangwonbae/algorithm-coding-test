5a5d8bd3342b2113649655730f4bca3979ac1284
Jangwonbae <84276491+Jangwonbae@users.noreply.github.com>
2026-02-07 14:12:02 +0900

    [level 1] Title: 덧칠하기, Time: 1.62 ms, Memory: 66.8 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/1/161989. 덧칠하기/덧칠하기.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(n: Int, m: Int, section: IntArray): Int {
+        var answer: Int = 0
+        var paint = 0
+        section.forEach{
+            it -> if(it > paint) {
+                paint = it+m-1
+                answer++
+                
+            } 
+        }
+        return answer
+    }
+}
