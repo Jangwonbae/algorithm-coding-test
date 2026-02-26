40b0f8d55868c74e5ba3919a871ddd404322629d
Jangwonbae <84276491+Jangwonbae@users.noreply.github.com>
2026-02-26 19:09:49 +0900

    [level 2] Title: 모음 사전, Time: 27.50 ms, Memory: 64 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/84512. 모음 사전/모음 사전.kt
+++ Kotlin/프로그래머스/2/84512. 모음 사전/모음 사전.kt
@@ -1,19 +1,17 @@
 class Solution {
-    val vowels = charArrayOf('A', 'E', 'I', 'O', 'U')
+    var vowels = charArrayOf('A', 'E', 'I', 'O', 'U')
     val allWords = mutableListOf<String>()
-
+    
     fun solution(word: String): Int {
-        generate("") // 모든 단어 생성 시작
-        return allWords.indexOf(word) // 0번째가 빈 문자열이므로 인덱스가 곧 순서
+        generate("")
+        return allWords.indexOf(word)
     }
-
     fun generate(current: String) {
-        if (current.length > 5) return // 최대 5글자 제한
-        
-        allWords.add(current)
-        
-        for (vowel in vowels) {
+            if (current.length > 5) return
+            allWords.add(current)
+            
+            for (vowel in vowels) {
             generate(current + vowel)
+            }
         }
-    }
 }
