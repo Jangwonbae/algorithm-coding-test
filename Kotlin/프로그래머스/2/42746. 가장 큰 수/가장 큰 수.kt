d14c8524114dd7aba0b29abd930870a29459662b
Jangwonbae <84276491+Jangwonbae@users.noreply.github.com>
2026-03-07 17:17:13 +0900

    [level 2] Title: 가장 큰 수, Time: 166.36 ms, Memory: 102 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.kt
+++ Kotlin/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.kt
@@ -4,10 +4,10 @@ class Solution {
         val strings = numbers.map { it.toString() }
         
         // 2. 두 수를 이어 붙여본 뒤 내림차순 정렬 (o2+o1 vs o1+o2)
-        val sorted = strings.sortedWith(Comparator { o1, o2 ->
+        val sorted = strings.sortedWith { o1, o2 -> 
             (o2 + o1).compareTo(o1 + o2)
-        })
-        
+            
+        }
         // 3. 모든 숫자가 0인 경우(예: [0, 0, 0]) "000"이 아닌 "0"을 반환하도록 처리
         if (sorted[0] == "0") return "0"
         
