e77e66c4291f1e8be727c5c4cd07768169e1874f
Jangwonbae <84276491+Jangwonbae@users.noreply.github.com>
2026-03-26 11:12:46 +0900

    [level 2] Title: 리코쳇 로봇, Time: 32.11 ms, Memory: 66 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.kt
+++ Kotlin/프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.kt
@@ -1,69 +1,74 @@
 import java.util.*
-
 class Solution {
-    // 이동 방향: 우, 좌, 하, 상
-    val dx = intArrayOf(0, 0, 1, -1)
-    val dy = intArrayOf(1, -1, 0, 0)
-
     fun solution(board: Array<String>): Int {
-        val n = board.size
-        val m = board[0].length
-        var start = IntArray(2)
+        var answer: Int = 0
+        val start = IntArray(2)
+        val goal = IntArray(2)
         
-        // 1. 시작점(R) 위치 찾기
-        for (i in 0 until n) {
-            for (j in 0 until m) {
-                if (board[i][j] == 'R') {
-                    start = intArrayOf(i, j)
+        for(i in 0..board.lastIndex) {
+            for(j in 0..board[0].lastIndex) {
+                when(board[i][j]) {
+                    'R' -> {
+                        start[0] = i
+                        start[1] = j
+                    }
+                    'G' -> {
+                        goal[0] = i
+                        goal[1] = j
+                    }
                 }
+                
             }
         }
-
-        return bfs(board, start, n, m)
+        answer = bfs(start, goal, board)
+        
+        
+        
+        return answer
     }
-
-    fun bfs(board: Array<String>, start: IntArray, n: Int, m: Int): Int {
+    fun bfs(start: IntArray, target: IntArray, board: Array<String>): Int {
+        val h = board.size
+        val w = board[0].length
+        
+        val xP = intArrayOf(-1, 1, 0, 0)
+        val yP = intArrayOf(0, 0, -1, 1)
+        
         val queue: Queue<Node> = LinkedList()
-        val visited = Array(n) { BooleanArray(m) }
-
-        queue.add(Node(start[0], start[1], 0))
-        visited[start[0]][start[1]] = true
-
-        while (queue.isNotEmpty()) {
-            val curr = queue.poll()
-
-            // 목표 지점(G)에 도달했는지 확인
-            if (board[curr.r][curr.c] == 'G') {
-                return curr.dist
+        val visited: Array<BooleanArray> = Array(h){BooleanArray(w)}
+        
+        val node = Node(start[0], start[1], 0)
+        queue.add(node)
+        visited[node.x][node.y] = true
+        
+        while(queue.isNotEmpty()) {
+            val (currX, currY, currDist) = queue.poll()
+            if (board[currX][currY] == 'G') {
+                return currDist
             }
-
-            // 4방향으로 미끄러지기
-            for (i in 0 until 4) {
-                var nr = curr.r
-                var nc = curr.c
-
-                // 범위를 벗어나지 않고 장애물(D)을 만나기 전까지 계속 이동
-                while (true) {
-                    val nextR = nr + dx[i]
-                    val nextC = nc + dy[i]
-
-                    if (nextR !in 0 until n || nextC !in 0 until m || board[nextR][nextC] == 'D') {
+            for(i in 0..3) {
+                var nx = currX
+                var ny = currY
+                
+                while(true) {
+                    val tx = nx + xP[i]
+                    val ty = ny + yP[i]
+                    
+                    if(tx !in 0 until h || ty !in 0 until w || board[tx][ty] == 'D'){
                         break
                     }
-                    nr = nextR
-                    nc = nextC
+                    nx = tx
+                    ny = ty
                 }
-
-                // 멈춘 위치가 방문하지 않았던 곳이라면 큐에 추가
-                if (!visited[nr][nc]) {
-                    visited[nr][nc] = true
-                    queue.add(Node(nr, nc, curr.dist + 1))
+                if (!visited[nx][ny]) {
+                    visited[nx][ny] = true
+                    queue.add(Node(nx, ny, currDist + 1))
                 }
             }
+            
         }
-
-        return -1 // 목표에 도달할 수 없는 경우
+        
+       
+        return -1
     }
-
-    data class Node(val r: Int, val c: Int, val dist: Int)
+    data class Node(val x: Int, val y: Int, val dist: Int)
 }
