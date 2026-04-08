6b860c465cdf2c64328538b9c444499dea0668ae
Jangwonbae <84276491+Jangwonbae@users.noreply.github.com>
2026-04-08 11:48:10 +0900

    [level 2] Title: 과제 진행하기, Time: 37.88 ms, Memory: 67.9 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/176962. 과제 진행하기/과제 진행하기.kt
+++ Kotlin/프로그래머스/2/176962. 과제 진행하기/과제 진행하기.kt
@@ -6,51 +6,45 @@ class Solution {
         val sortedPlans = plans.sortedBy{it[1]}
         
         val stack = Stack<Pair<String, Int>>()
-        var subject = ""
-        var startTime = 0
+        var currentSub = ""
+        var currentTime = 0
         var endTime = 0
         for(plan in sortedPlans) {
-            var sub = plan[0]
-            var time = convertMin(plan[1])
-            var min = plan[2].toInt()
+            var nextSub = plan[0]
+            var nextTime = convertMin(plan[1])
+            var nextDuration = plan[2].toInt()
             
-            if(subject.isEmpty()) {
-                subject = sub
-                startTime = time
-                endTime = startTime + min
+            if(currentSub.isEmpty()) {
+                currentSub = nextSub
+                currentTime = nextTime
+                endTime = currentTime + nextDuration
+                continue
+            } 
+            if(endTime > nextTime) {
+                val remainTime = endTime - nextTime
+                stack.push(Pair(currentSub, remainTime))
+            } else if(endTime == nextTime) {
+                answer.add(currentSub)
             } else {
-                if(endTime > time) {
-                    val remainTime = endTime - time
-                    stack.push(Pair(subject, remainTime))
-                    subject = sub
-                    startTime = time
-                    endTime = startTime + min
-                } else if(endTime == time) {
-                    answer.add(subject)
-                    subject = sub
-                    startTime = time
-                    endTime = startTime + min
-                } else {
-                    answer.add(subject)
-                    var availableTime = time - endTime
-                    while(availableTime > 0 && stack.isNotEmpty()) {
-                        var (remainSub, remainTime) = stack.pop()
-                        if(availableTime >= remainTime) {
-                            availableTime -= remainTime
-                            answer.add(remainSub)
-                        } else {
-                            remainTime = remainTime - availableTime
-                            availableTime = 0
-                            stack.push(Pair(remainSub, remainTime))
-                        }
+                answer.add(currentSub)
+                var availableTime = nextTime - endTime
+                while(availableTime > 0 && stack.isNotEmpty()) {
+                    var (remainSub, remainTime) = stack.pop()
+                    if(availableTime >= remainTime) {
+                        availableTime -= remainTime
+                        answer.add(remainSub)
+                    } else {
+                        remainTime = remainTime - availableTime
+                        availableTime = 0
+                        stack.push(Pair(remainSub, remainTime))
                     }
-                    subject = sub
-                    startTime = time
-                    endTime = startTime + min
                 }
-            }   
+            }
+            currentSub = nextSub
+            currentTime = nextTime
+            endTime = currentTime + nextDuration
         }
-        answer.add(subject)
+        answer.add(currentSub)
         while(stack.isNotEmpty()) {
             var (remainSub, remainTime) = stack.pop()
             answer.add(remainSub)
