534b44c6bbd388d15fc3e244fcb04cc5ab625b91
Jangwonbae <84276491+Jangwonbae@users.noreply.github.com>
2026-04-09 11:11:37 +0900

    [level 2] Title: N-Queen, Time: 166.98 ms, Memory: 61.1 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/12952. N－Queen/N－Queen.kt
+++ Kotlin/프로그래머스/2/12952. N－Queen/N－Queen.kt
@@ -1,8 +1,34 @@
 class Solution {
+    var answer = 0
+    private lateinit var cols: IntArray
     fun solution(n: Int): Int {
+        cols = IntArray(n)
+        backtrack(0, n)
         
-        var answer = intArrayOf(1,0,0,2,10,4,40,92,352,724,2680,14200)
-        
-        return answer[n-1]
+        return answer
+    }
+    fun backtrack(row: Int, n: Int) {
+        if(row == n) {
+            answer++
+            return
+        }
+        for(col in 0 until n) {
+            if(isSafe(row, col)) {
+                cols[row] = col
+                backtrack(row + 1, n)
+            }
+        }
+    }
+    fun isSafe(row: Int, col: Int): Boolean {
+        for (i in 0 until row) {
+            // 1. 같은 열 확인: cols[i] == col
+            // 2. 대각선 확인: 행의 차이 == 열의 차이
+            val colDiff = Math.abs(col - cols[i])
+            val rowDiff = row - i
+            if (cols[i] == col || colDiff == rowDiff) {
+                return false
+            }
+        }
+        return true
     }
 }
