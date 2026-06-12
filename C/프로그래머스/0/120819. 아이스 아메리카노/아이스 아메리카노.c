359fa87851a1a844299499026a2a512b865f16f2
Jangwonbae <84276491+Jangwonbae@users.noreply.github.com>
2026-06-12 13:57:19 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 4.06 MB -BaekjoonHub

--- C/프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.c
+++ C/프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.c
@@ -5,7 +5,7 @@
 int* solution(int money) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int* answer = (int*)malloc(sizeof(int)*2);
-    answer[0] = money/5500;
-    answer[1] = money%5500;
+    *answer = money/5500;
+    *(answer+1) = money%5500;
     return answer;
 }
