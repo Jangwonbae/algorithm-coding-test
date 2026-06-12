60cb3bff16dcc7e3dbdeb68eed436c91652e4b26
Jangwonbae <84276491+Jangwonbae@users.noreply.github.com>
2026-06-12 13:49:38 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 3.82 MB -BaekjoonHub

---
+++ C/프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int slice, int n) {
+    int answer = 0;
+    if(n<= slice) {
+        answer = 1;
+    } else {
+        answer = n/slice + (n%slice==0? 0:1);
+    }
+    return answer;
+}
